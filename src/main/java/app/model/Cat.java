cc10557001ba76f9822044921d3bb849d13fa39b
strorikaster <strorikaster@gmail.com>
2021-10-05 22:43:09 +0300

    Add files via upload

---
+++ src/main/java/app/model/Cat.java
@@ -0,0 +1,13 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Cat";
+    }
+}
